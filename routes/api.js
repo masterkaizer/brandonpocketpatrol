5699a595621fa51fb2238823c30f65895b01c41d
Your Name <brandon.sellam@gmail.com>
2020-06-12 21:12:42 +0200

    pocket cops 4 ever

---
+++ routes/api.js
@@ -0,0 +1,40 @@
+const router = require("express").Router();
+const Transaction = require("../models/transaction.js");
+
+
+//api to create transaction
+router.post("/api/transaction", ({ body }, res) => {
+  Transaction.create(body)
+    .then(dbTransaction => {
+      res.json(dbTransaction);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+//api to create transaction in bulk
+router.post("/api/transaction/bulk", ({ body }, res) => {
+  Transaction.insertMany(body)
+    .then(dbTransaction => {
+      res.json(dbTransaction);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+
+//api to get transaction
+router.get("/api/transaction", (req, res) => {
+  Transaction.find({})
+    .sort({ date: -1 })
+    .then(dbTransaction => {
+      res.json(dbTransaction);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+module.exports = router;
